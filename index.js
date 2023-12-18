b765ccd1f6c2a33941539c042a57409008a535c8
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-18 22:26:14 +0500

    added Handlebars and fix code

--- index.js
+++ index.js
@@ -2,17 +2,25 @@ import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { engine } from "express-handlebars";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
+
 const app = express();
+
+
+
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
+
 app.get("/", (req, res) => {
   res.render("index");
 });
+
 app.get("/about", (req, res) => {
   res.render("about");
 });
+
 const PORT = process.env.PORT || 4100;
 app.listen(PORT, () => console.log(`Server is running on POST: ${PORT}`));
