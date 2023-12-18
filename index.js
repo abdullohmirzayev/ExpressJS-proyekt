ae9c7c9a4d164a2f3a8b467fe2f8f11edb28e002
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-18 20:18:17 +0500

    added Rounting and fix code

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const app = express();
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "index.html"));
+});
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "views", "about.html"));
+});
+const PORT = process.env.PORT || 4100;
+app.listen(PORT, () => console.log(`Server is running on POST: ${PORT}`));
