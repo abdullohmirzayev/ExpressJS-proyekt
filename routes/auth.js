bc6580803b883a31f0097572785b273f9fe9c113
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-19 16:45:05 +0500

    added #6 Dynamic title

--- routes/auth.js
+++ routes/auth.js
@@ -2,11 +2,17 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", {
+    title: "Login | Abu",
+    isLogin: true,
+  });
 });
 
 router.get("/register", (req, res) => {
-  res.render("register");
+  res.render("register", {
+    title: "Register | Abu",
+    isRegister: true,
+  });
 });
 
 export default router;
--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,24 @@
 import { Router } from "express";
 const router = Router();
+
 router.get("/", (req, res) => {
-  res.render("index");
+  res.render("index", {
+    title: "Boom shop | Abu",
+  });
 });
 
 router.get("/products", (req, res) => {
-  res.render("products");
+  res.render("products", {
+    title: "Products | Abu",
+    isProducts: true,
+  });
 });
 
 router.get("/add", (req, res) => {
-  res.render("add");
+  res.render("add", {
+    title: "Add products | Abu",
+    isAdd: true,
+  });
 });
+
 export default router;
