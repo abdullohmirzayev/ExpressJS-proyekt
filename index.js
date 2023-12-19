ae2fe16ccf5ba1b306614ee913773135b962559c
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-19 18:00:46 +0500

    added #8 Static folder

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const hbs = create({
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
+
+app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.use(AuthRoutes);
