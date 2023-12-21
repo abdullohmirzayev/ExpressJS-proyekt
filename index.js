f323b0df952fd0a9cc26a0a22753bc0886620fc6
Abdulloh-Mirzayev <abdullohmirzayev7570@gmail.com>
2023-12-21 16:45:04 +0500

    added #18 Dynamic navbar, Cookie

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import { create } from "express-handlebars";
 import mongoose from "mongoose";
 import flash from "connect-flash";
 import session from "express-session";
+import varMiddleware from "./middieware/var.js";
+import cookieParser from "cookie-parser";
 import * as dotenv from "dotenv";
 
 // Routes
@@ -25,10 +27,12 @@ app.set("views", "./views");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cookieParser());
 app.use(
   session({ secret: "Abdulloh", resave: false, saveUninitialized: false })
 );
 app.use(flash());
+app.use(varMiddleware);
 
 app.use(AuthRoutes);
 app.use(ProductsRoutes);
